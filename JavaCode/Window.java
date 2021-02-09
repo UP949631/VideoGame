c862523e5fa64e3e0fa029eeeb5e700ebad8ca53
UP959856 <up959856@myport.ac.uk>
2021-02-09 18:42:57 +0000

    Create Window.java

---
+++ JavaCode/Window.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends Canvas {
+
+    public Window(int width, int height, String title, Game game){
+
+        JFrame frame = new JFrame(title);
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit Button on Window.
+        frame.setResizable(false); //Prevent resizing of the window.
+        frame.setLocationRelativeTo(null); //Make window start in middle of screen to make it seem nicer.
+        frame.add(game);
+        frame.setVisible(true);
+        game.start();
+
+    }
+
+}
